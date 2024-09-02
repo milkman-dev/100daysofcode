37ce38a8280eddafba0a63125896b8d9dc280caa
Milkman <loganescobarc@gmail.com>
2024-09-01 19:44:39 -0600

    day 1 - output and input

---
+++ cmd/band-name-gen/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bandNameGenerator() string {
+	var city, pet, bandName string
+	fmt.Println("Welcome to the band name generator!")
+	fmt.Println("What's the name of the city you grew up in?")
+	fmt.Scan(&city)
+	fmt.Println("What's your pet's name?")
+	fmt.Scan(&pet)
+	bandName = fmt.Sprintf("Your band name could be %s %s", city, pet)
+
+	return bandName
+}
+
+func main() {
+	fmt.Println(bandNameGenerator())
+}
