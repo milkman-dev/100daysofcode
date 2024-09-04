7f226b2c8945d476aa78822ab2ef3233e194b3b3
Milkman <loganescobarc@gmail.com>
2024-09-04 00:15:56 -0600

    Added the adventure game

---
+++ cmd/adventure-game/main.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+func adventureGame() {
+
+	type Choices struct {
+		directionChoice string
+		lakeChoice      string
+		doorChoice      string
+	}
+
+	gameChoices := Choices{}
+
+	fmt.Println(`
+******************************
+⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣
+******************************`)
+	fmt.Println("Welcome to treasure island!")
+	fmt.Println("Your mission is to find the treasure")
+	fmt.Println("You're at a cross road, where do you want to go?")
+	fmt.Print("Type 'left' or 'right': ")
+	fmt.Scan(&gameChoices.directionChoice)
+
+	if gameChoices.directionChoice != "left" {
+		if gameChoices.directionChoice == "right" {
+			fmt.Println("Bro you ded")
+			return
+		} else {
+			fmt.Println("That wasn't even an option wtf")
+			return
+		}
+	}
+
+	fmt.Println("You've come to a lake and there is an island in the middle of the lake")
+	fmt.Print("Type 'wait' or 'swim': ")
+	fmt.Scan(&gameChoices.lakeChoice)
+
+	if gameChoices.lakeChoice != "wait" {
+		fmt.Println("You got eaten by a big ahh shark, rlly?")
+		return
+	}
+
+	fmt.Println("You arrive at the island unharmed. There is a house with 3 doors")
+	fmt.Print("One red, one yellow and one blue. Which colour do you choose?: ")
+	fmt.Scan(&gameChoices.doorChoice)
+
+	if gameChoices.doorChoice == "red" {
+		fmt.Println("There was a room full of fire")
+	} else if gameChoices.doorChoice == "blue" {
+		fmt.Println("You got eaten by some beasts")
+	} else if gameChoices.doorChoice == "yellow" {
+		fmt.Println("You found the treasure!")
+	}
+
+	fmt.Println("Game over")
+	return
+}
+
+func main() {
+	adventureGame()
+}
